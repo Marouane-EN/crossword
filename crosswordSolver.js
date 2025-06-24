9e4bc55024b2b4735a18a6e5f9aaffb2cc3961af
Marouane-EN <enmaroine443@gmail.com>
2025-06-24 10:11:29 +0100

    add the solve func

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,4 +1,10 @@
-import { duplication, checklength,checklines,validPuzzle,checkalpha } from "./utils.js";
+import {
+  duplication,
+  checklength,
+  checklines,
+  validPuzzle,
+  checkalpha,
+} from "./utils.js";
 function crosswordSolver(words, puzzle) {
   if (
     !Array.isArray(words) ||
@@ -13,26 +19,60 @@ function crosswordSolver(words, puzzle) {
   if (checklength(puzzle) !== joindWord.length) {
     return "Error length";
   }
-  if(!checklines(puzzle)|| !validPuzzle(puzzle)){
-    return "Eroror checklines or validpuzzle"
+  if (!checklines(puzzle) || !validPuzzle(puzzle)) {
+    return "Eroror checklines or validpuzzle";
   }
-  if (!checkalpha(words)){
-    return "Error checkalpha"
+  if (!checkalpha(words)) {
+    return "Error checkalpha";
   }
   return "dsdf";
 }
+function solver(matrix, solved, words, index, row, col) {
+  if (!isNaN(matrix[row][col])) {
+    let num = Number(matrix[row][col]);
+    if (num > 0) {
+      num = num - 1;
+      matrix[row][col] = String(num);
+      if (horizental(matrix[row], col, words[index])) {
+      }
+    }
+  }
+}
 
+function horizental(row, col, word) {
+  let count = 0;
+  for (let i = col; i < row.length; i++) {
+    if (row[i] == ".") {
+      break;
+    }
+    if (col > 0 && row[col - 1] != ".") {
+      return false;
+    }
+    if (count == word.length && (i != row.length - 1 || row[i + 1] != ".")) {
+      return false;
+    }
+    if (!isNaN(row[i])) {
+      count++;
+    } else {
+      if (row[i] == word[i]) {
+        count++;
+      } else {
+        return false;
+      }
+    }
+  }
+  return count == word.length;
+}
 
 const puzzle = `222
-222`
-const words = ['caa', 'alan', 'ciao', 'anta']
+222`;
+const words = ["caa", "alan", "ciao", "anta"];
 
 // function recursive(c){
 //     if(c>10){
 //         return ;
 //     }
-    
-    
+
 //     recursive(c+1)
 //     console.log(c);
 // }
